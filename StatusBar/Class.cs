44b8c5cb3f8b80234be8289229b6c56a6daff0f3
miso <misodengaku@gmail.com>
2012-04-10 06:03:16 +0900

    first commit

---
+++ StatusBar/Class.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using Microsoft.Phone.Shell;
+
+namespace StatusBarUtil
+{
+    public class StatusBar
+    {
+        static ProgressIndicator sysProg = new ProgressIndicator();
+
+        #region フィールド
+        static public bool isVisible
+        {
+            get
+            {
+                return sysProg.IsVisible;
+            }
+        }
+
+        static public double Value
+        {
+            get
+            {
+                return sysProg.Value;
+            }
+        }
+
+        static public string Text
+        {
+            get
+            {
+                return sysProg.Text;
+            }
+        }
+
+        static public bool isIndeterminate
+        {
+            get
+            {
+                return sysProg.IsIndeterminate;
+            }
+        }
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        /// ステータスバーにメッセージを表示します。
+        /// </summary>
+        /// <param name="t">ステータスバーを設定するページ（たぶんthis）</param>
+        /// <param name="mes">表示するメッセージ</param>
+        public static void ShowStatusBar(object t, string mes)
+        {
+            sysProg.Text = mes;
+            sysProg.IsIndeterminate = false;
+            sysProg.IsVisible = true;
+            SystemTray.SetProgressIndicator((DependencyObject)t, sysProg);
+        }
+
+        /// <summary>
+        /// ステータスバーにメッセージと処理が進行中であることをを表示します。
+        /// </summary>
+        /// <param name="t">ステータスバーを設定するページ（たぶんthis）</param>
+        /// <param name="mes">表示するメッセージ</param>
+        /// <param name="isIndeterminate">進行中かどうか</param>
+        public static void ShowStatusBar(object t, string mes, bool isIndeterminate)
+        {
+            sysProg.Text = mes;
+            sysProg.IsIndeterminate = isIndeterminate;
+            sysProg.IsVisible = true;
+            sysProg.Value = 0;
+            SystemTray.SetProgressIndicator((DependencyObject)t, sysProg);
+        }
+
+        /// <summary>
+        /// ステータスバーにメッセージと処理が進行中であることをを表示します。
+        /// </summary>
+        /// <param name="t">ステータスバーを設定するページ（たぶんthis）</param>
+        /// <param name="mes">表示するメッセージ</param>
+        /// <param name="value">進捗度（0-100）</param>
+        public static void ShowStatusBar(object t, string mes, int value)
+        {
+            sysProg.Text = mes;
+            sysProg.IsIndeterminate = false;
+            sysProg.IsVisible = true;
+            sysProg.Value = value;
+            SystemTray.SetProgressIndicator((DependencyObject)t, sysProg);
+        }
+
+        /// <summary>
+        /// ステータスバーの表示を制御します。
+        /// </summary>
+        /// <param name="t">ステータスバーを設定するページ（たぶんthis）</param>
+        /// <param name="isShow">表示するかどうか</param>
+        public static void ShowStatusBar(object t, bool isShow)
+        {
+            sysProg.IsVisible = isShow;
+            SystemTray.SetProgressIndicator((DependencyObject)t, sysProg);
+        }
+        #endregion
+    }
+}
---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using StatusBarUtil;
+using System.Threading;
+
+namespace TestApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            StatusBar.ShowStatusBar(this, "Test1");
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            StatusBar.ShowStatusBar(this, "Test2", true);
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                StatusBar.ShowStatusBar(this, "Test3", i);
+                i++;
+                //Thread.Sleep(50);
+            }
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            if (StatusBar.isVisible)
+                StatusBar.ShowStatusBar(this, !StatusBar.isVisible);
+        }
+    }
+}
